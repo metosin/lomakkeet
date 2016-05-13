36ea2c5116c59aba2d693a3e14786228b468a135
Juho Teperi <juho.teperi@iki.fi>
2016-05-13 12:19:06 +0300

    Build example from a513b46d2714c222805b182f7cc3716a6213f892.

--- main.out/adzerk/boot_reload.cljs
+++
@@ -1,2 +0,0 @@
-(ns adzerk.boot-reload (:require [adzerk.boot-reload.client :as client] example.main))
-(client/connect "ws://localhost:40232" {:on-jsload (fn* [] (example.main/restart!))})
--- main.out/boot/cljs/main21147.cljs
+++
@@ -1,1 +0,0 @@
-(ns boot.cljs.main21147 (:require adzerk.boot-cljs-repl adzerk.boot-reload example.main))
