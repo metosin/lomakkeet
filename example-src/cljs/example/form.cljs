a38de41e7043bea16f293ae5c7f18084fabf711a
Juho Teperi <juho.teperi@iki.fi>
2016-05-15 22:13:59 +0300

    Add cancel and submit buttons to the example

--- example-src/cljs/example/form.cljs
+++ example-src/cljs/example/form.cljs
@@ -15,22 +15,38 @@
 (defn simple-form' [state]
   (let [form (l/create-form state)]
     (fn [_]
-      [:div.container-fluid
-       [:div.row
-        [l/input form "Name" [:name]]
-        [l/input form "Email" [:email]]]
-       [:div.row
-        [l/complete form "Postalcode" [:postalcode] {:item->value #(select-keys % [:code :name])
-                                                     :item->text #(str (:code %) " - " (:name %))
-                                                     :item->key :code
-                                                     :value->text (fn [_ v] (if v (str (:code v) " - " (:name v))))
-                                                     :search-fields [:code :name]
-                                                     :items [{:code "33720" :name "Tampere"}
-                                                             {:code "33100" :name "Tampere"}]}]
-        [l/select form "Language" [:language]
-         [{:value "fi" :text "Finnish"}
-          {:value "en" :text "English"}]
-         {:empty-option? true}]]])))
+      [:form
+       {:on-submit (fn [e]
+                     (.preventDefault e)
+                     (if-not (l/errors? @state)
+                       (js/alert "Form sent")))}
+       [:div.container-fluid
+        [:div.row
+         [l/input form "Name" [:name]]
+         [l/input form "Email" [:email]]]
+        [:div.row
+         [l/complete form "Postalcode" [:postalcode] {:item->value #(select-keys % [:code :name])
+                                                      :item->text #(str (:code %) " - " (:name %))
+                                                      :item->key :code
+                                                      :value->text (fn [_ v] (if v (str (:code v) " - " (:name v))))
+                                                      :search-fields [:code :name]
+                                                      :items [{:code "33720" :name "Tampere"}
+                                                              {:code "33100" :name "Tampere"}]}]
+         [l/select form "Language" [:language]
+          [{:value "fi" :text "Finnish"}
+           {:value "en" :text "English"}]
+          {:empty-option? true}]]
+        [:div.row
+         [:div.col-sm-12
+          [:div.btn-toolbar
+           [:button.btn.btn-default
+            {:type "button"
+             :on-click (fn [e]
+                         (swap! state (comp l/validate l/reset)))}
+            "Cancel"]
+           [:button.btn.btn-success
+            {:type "submit"}
+            "Submit"]]]]]])))
 
 (dc/defcard-rg simple-form
   (fn [state _]
